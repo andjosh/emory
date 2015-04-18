6ffb2659ba73de082b524e67be56ce50aa1b39ec
andjosh <email@andjosh.com>
2015-04-18 16:10:59 -0500

    saving?

---
+++ models/message.js
@@ -0,0 +1,21 @@
+/**
+  * Message
+  *
+  */
+var mongoose = require('mongoose'),
+    Schema = mongoose.Schema,
+    Message = new Schema({
+        subject: {
+            type: String, 
+            default: ''
+        },
+        tags: [String],
+        text: String,
+        raw: String,
+        html: String,
+        from: String,
+        to: String,
+        created: (new Date())
+    });
+
+module.exports = mongoose.model('Message', Message);
--- routes/url.js
+++ routes/url.js
@@ -6,9 +6,11 @@ module.exports = function (app, route){
 
 app.use(route.post('/send', function* () {
     'use strict';
-    yield {};
-    console.log(this.request.body.fields);
-    console.log(JSON.parse(this.request.body.fields.mandrill_events));
+    var array = yield JSON.parse(
+            this.request.body.fields.mandrill_events
+        );
+
+    this.body = array.map(Parser);
 }));
 
 };
